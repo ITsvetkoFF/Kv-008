6d0a9e2a617d94ca42e1d9270f439e408a6014f3
cmgc <unlesnick@gmail.com>
2015-04-29 22:12:50 +0300

    Update settings

--- ecomap/api/v1_0/settings.py
+++ ecomap/api/v1_0/settings.py
@@ -3,10 +3,10 @@
 
 class LocalSettings(object):
     DEBUG = True
-    BIND_PORT = 8080
-    BIND_ADDR = '127.0.0.1'
+    BIND_PORT = 8001
+    BIND_ADDR = '0.0.0.0'
     PSQL_LOGIN = 'ecouser'
-    PSQL_PASSWORD = '3c0map$008kv'
+    PSQL_PASSWORD = '3c0map$008kv' # just for testing. will be updated in production.
     PSQL_HOST = 'localhost'
     PSQL_PORT = 5432
     PSQL_DBNAME = 'ecomap_db'
