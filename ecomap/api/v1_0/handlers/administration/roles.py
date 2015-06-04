a9b78bdb2bad866361111951b3d4f3e194a0b8bf
Mike Bohdan <darkcore27@gmail.com>
2015-06-04 22:31:28 +0300

    Users API is ready.

--- ecomap/api/v1_0/handlers/administration/roles.py
+++ ecomap/api/v1_0/handlers/administration/roles.py
@@ -1,4 +1,5 @@
 import tornado.escape
+import tornado.web
 
 from api.v1_0.handlers.base import BaseHandler
 from api.v1_0.models import Role
@@ -6,11 +7,13 @@ from api.v1_0.models import Role
 
 class RolesHandler(BaseHandler):
 
+    @tornado.web.authenticated
     def get(self, *args, **kwargs):
         temp = tornado.escape.json_encode({'roles': [{'name': role.name,
                                                       'id': role.id} for role in self.sess.query(Role).all()]})
         self.write(temp)
 
+    @tornado.web.authenticated
     def put(self):
         query = Role(name=self.request.arguments['role_name'])
         self.sess.add(query)
