f9333ed1f2bae661f1d15e51976ff4c0b4991076
Bigun <begun.aleksandr@gmail.com>
2015-06-12 19:20:09 +0300

    Bigun commit

--- ecomap/api/v1_0/handlers/problem.py
+++ ecomap/api/v1_0/handlers/problem.py
@@ -1,7 +1,10 @@
 from datetime import datetime
-
+from tornado import escape
+from wtforms_json import InvalidData
 from api.v1_0.handlers.base import BaseHandler
-from api.v1_0.models import VotesActivity
+from api.v1_0.models import VotesActivity, DetailedProblem, Problem, \
+    ProblemsActivity
+from api.v1_0.forms.validation_json import  ProblemForm
 
 
 class ProblemsHandler(BaseHandler):
@@ -10,15 +13,125 @@ class ProblemsHandler(BaseHandler):
         not None, get the problem identified by problem_id and write it to
         the client."""
 
-    def post(self):
-        """Store a new problem to the database."""
+        if problem_id != None:
+            problem = self.sess.query(DetailedProblem).get(int(problem_id))
+            if problem != None:
+                problem_data = dict()
+                for c in problem.__table__.columns:
+                    if isinstance(getattr(problem, c.name), datetime):
+                        problem_data[c.name] = str(getattr(problem, c.name))
+                    else:
+                        problem_data[c.name] = getattr(problem, c.name)
+                self.write(problem_data)
+            else:
+                self.send_error(404,
+                                message='Problem not found for the given id.')
+        else:
+            self.send_error(404, message='Problem not found for the given id.')
 
+    def post(self, problem_id):
+        """Store a new problem to the database."""
+        if self.get_action_modifier() != 'NONE':
+            try:
+                form = ProblemForm.from_json(self.request.arguments,
+                                             skip_unknown_keys=False)
+            except InvalidData as e:
+                message = e.args
+                self.send_error(400, message=message)
+            for i in form.__dict__.keys():
+                print form.__dict__[i]
+
+            if form.validate():
+
+                problem = Problem(title=self.request.arguments['title'],
+                                  content=self.request.arguments['content'],
+                                  proposal=self.request.arguments['proposal'],
+                                  severity=self.request.arguments['severity'],
+                                  status=self.request.arguments['status'],
+                                  location=self.create_location(),
+                                  problem_type_id=self.request.arguments[
+                                      'problem_type_id'],
+                                  region_id=self.request.arguments['region_id'])
+                self.sess.add(problem)
+                self.sess.commit()
+                activity = ProblemsActivity(problem_id=problem.id,
+                                            data=escape.json_decode(
+                                                self.request.body),
+                                            user_id=self.get_current_user(),
+                                            datetime=datetime.utcnow(),
+                                            activity_type="ADDED")
+                self.sess.add(activity)
+                self.sess.commit()
+            else:
+                message = form.errors
+                self.send_error(400, message=message)
+        else:
+            message = 'You have not permission to adde problem_id.'
+            self.send_error(400, message=message)
 
     def put(self, problem_id):
-        """Edit the problem, identified by problem_id."""
+        modifire = self.get_action_modifier()
+        user_id = self.sess.query(ProblemsActivity.user_id). \
+            filter_by(problem_id=problem_id, activity_type='ADDED').first()
+
+        if modifire == 'ANY' or (
+                        modifire == 'OWN' and user_id[
+                    0] == self.get_current_user()):
+            try:
+                form = ProblemForm.from_json(self.request.arguments,
+                                             skip_unknown_keys=False)
+            except InvalidData as e:
+                message = e.message
+                self.send_error(400, message=message)
+
+            if form.validate():
+                self.request.arguments['location'] = self.create_location()
+                self.request.arguments.pop('Latitude')
+                self.request.arguments.pop('Longtitude')
+                self.request.arguments['id'] = problem_id
+                self.sess.query(Problem).filter_by(id=int(problem_id)). \
+                    update(self.request.arguments)
+
+                self.sess.commit()
+
+                activity = ProblemsActivity(problem_id=int(problem_id),
+                                            data=escape.json_decode(
+                                                self.request.body),
+                                            user_id=self.get_current_user(),
+                                            datetime=datetime.utcnow(),
+                                            activity_type="UPDATED")
+                self.sess.add(activity)
+                self.sess.commit()
+            else:
+                message = form.errors
+                self.send_error(400, message=message)
+        else:
+            message = 'You have not permission to update.'
+            self.send_error(400, message=message)
 
     def delete(self, problem_id):
         """Delete a problem from the database by given problem id."""
+        modifire = self.get_action_modifier()
+        user_id = self.sess.query(ProblemsActivity.user_id). \
+            filter_by(problem_id=problem_id, activity_type='ADDED').first()
+
+        if modifire == 'ANY' or (
+                        modifire == 'OWN' and user_id[
+                    0] == self.get_current_user()):
+
+            activity = ProblemsActivity(problem_id=int(problem_id),
+                                        data=None,
+                                        user_id=self.get_current_user(),
+                                        datetime=datetime.utcnow(),
+                                        activity_type='REMOVED')
+            self.sess.add(activity)
+            self.sess.commit()
+
+            self.sess.query(Problem).filter_by(id=int(problem_id)).delete()
+            self.sess.commit()
+        else:
+            message = 'You have not permission to remove.'
+            self.send_error(400, message=message)
 
 
 class ProblemVoteHandler(BaseHandler):
@@ -26,9 +139,8 @@ class ProblemVoteHandler(BaseHandler):
         new_vote = VotesActivity(
             problem_id=int(problem_id),
             user_id=self.current_user,
-            datetime=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            datetime=datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
         )
 
         self.sess.add(new_vote)
         self.sess.commit()
-
