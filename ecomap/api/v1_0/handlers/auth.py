bd71f603d01a442dec1a322c066064b316876be0
yevhen-m <yvhn.yvhn@gmail.com>
2015-06-17 18:24:06 +0300

    allproblems.py

--- ecomap/api/v1_0/handlers/auth.py
+++ ecomap/api/v1_0/handlers/auth.py
@@ -1,174 +1,108 @@
 import tornado.gen
-import tornado.web
-from tornado.auth import GoogleOAuth2Mixin, FacebookGraphMixin
+import tornado.auth
 import tornado.escape
 
-from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
-
 from api.v1_0.handlers.base import BaseHandler
-from api.v1_0.models import User
-import json
+from api.v1_0.bl.auth_logic import *
 
 
 class RegisterHandler(BaseHandler):
     def post(self):
-        try:
-            stored_user = self.sess.query(User).filter_by(
-                email=self.request.arguments['email']).one()
-
-            if stored_user:
-                self.send_error(400, message='Registration failed. '
-                                             'Email already in use.')
-                return
-
-        except NoResultFound:
-            pass
-
-        except MultipleResultsFound:
-            self.send_error(message='Registration failed. Multiple '
-                                    'users found for the given email.')
+        user_data = decode_json(self)
+        if user_data is None:
+            # there was an exception while decoding
+            # response message was sent
+            return
+        form = create_user_register_form(user_data, self)
+        if form is None:
+            # there was an exception while checking fields
+            # response message was sent
             return
 
-        new_user = User(
-            first_name=self.request.arguments['first_name'],
-            last_name=self.request.arguments['last_name'],
-            email=self.request.arguments['email'],
-            region_id=self.request.arguments['region_id'],
-            password=self.request.arguments['password']
-        )
-
-        self.sess.add(new_user)
-        self.sess.flush()
-        self.set_cookie('user_id', str(new_user.id))
-        self.sess.commit()
+        if form.validate():
+            new_user_id = store_registered_new_user(self, user_data)
+            set_cookie_with_login_informer(self, new_user_id)
+        else:
+            self.send_error(400, message='Invalid data sent.',
+                            errors=form.errors)
 
 
 class FacebookAuthHandler(BaseHandler, tornado.auth.FacebookGraphMixin):
     @tornado.gen.coroutine
     def get(self):
-
-        uri = 'http://{}:{}{}'.format(
-            self.settings['hostname'],
-            self.settings['bind_port'],
-            self.reverse_url('facebook_auth')
-        )
+        uri = get_absolute_redirect_uri(self, 'fb_auth')
 
         if self.get_argument('code', None):
-            user = yield self.get_authenticated_user(
+            user_data = yield self.get_authenticated_user(
                 redirect_uri=uri,
-                client_id=self.settings['facebook_oauth']['key'],
-                client_secret=self.settings['facebook_oauth']['secret'],
-                code=self.get_argument('code'),
+                client_id=self.settings['fb_oauth']['key'],
+                client_secret=self.settings['fb_oauth']['secret'],
+                code=self.get_argument('code')
             )
-
-            if not user:
-                self.clear_all_cookies()
-                self.send_error(500, message='User facebook '
-                                             'authentication failed.')
+            if not user_data:
+                handle_client_failure('Facebook', 'Authentication failure.')
                 return
 
-            # check if this user is stored in the database
-            try:
-                stored_user = self.sess.query(User).filter_by(
-                    facebook_id=user['id']).one()
-                self.set_cookie('user_id', str(stored_user.id))
-                return
-            except NoResultFound:
-                pass
-            except MultipleResultsFound:
-                self.send_error(500, 'Multiple users found for the given id.')
+            user = get_stored_user_by_3rd_party_id(
+                self, 'facebook', user_data['id']
+            )
+            if user:
+                set_cookie_with_login_informer(self, user.id)
                 return
 
             user_profile = yield self.facebook_request(
                 path='/me',
-                access_token=user['access_token'],
+                access_token=user_data['access_token']
             )
-
             if not user_profile:
-                self.send_error(500, message='User facebook profile '
-                                             'access failed.')
+                handle_client_failure('Facebook', 'Profile access failure.')
+                return
 
-            # save user to the database
-            new_user = User(
-                first_name=user_profile['first_name'],
-                last_name=user_profile['last_name'],
-                email=user_profile['email'],
-                facebook_id=user_profile['id']
-            )
-
-            self.sess.add(new_user)
-            self.flush()
-            self.set_cookie('user_id', str(new_user.id))
-            self.sess.commit()
+            new_user_id = store_fb_new_user(self, user_profile)
+            set_cookie_with_login_informer(self, str(new_user_id))
             return
 
         yield self.authorize_redirect(
             redirect_uri=uri,
-            client_id=self.settings['facebook_oauth']['key'],
+            client_id=self.settings['fb_oauth']['key'],
             extra_params={'scope': 'email'}
         )
 
 
-class GoogleAuthHandler(BaseHandler, GoogleOAuth2Mixin):
+class GoogleAuthHandler(BaseHandler, tornado.auth.GoogleOAuth2Mixin):
     @tornado.gen.coroutine
     def get(self):
+        uri = get_absolute_redirect_uri(self, 'google_auth')
 
-        uri = 'http://{}:{}{}'.format(
-            self.settings['hostname'],
-            self.settings['bind_port'],
-            self.reverse_url('google_auth')
-        )
-
-        if self.get_argument('code', False):
-            user = yield self.get_authenticated_user(
+        if self.get_argument('code', None):
+            user_data = yield self.get_authenticated_user(
                 redirect_uri=uri,
                 code=self.get_argument('code')
             )
-
-            if not user:
-                self.clear_all_cookies()
-                self.send_error(500, message='User google '
-                                             'authentication failed.')
+            if not user_data:
+                handle_client_failure('Google', 'Authentication failure.')
                 return
 
-            access_token = str(user['access_token'])
-            http_client = self.get_auth_http_client()
-            response = yield http_client.fetch(
+            # google request for user profile
+            response = yield self.get_auth_http_client().fetch(
                 'https://www.googleapis.com/oauth2/v1/userinfo?access_token'
-                '=' + access_token)
+                '=' + str(user_data['access_token']))
 
             if not response:
-                self.clear_all_cookies()
-                self.send_error(500, message='User google '
-                                             'profile access failed.')
+                handle_client_failure('google', 'profile access failure')
                 return
 
             user_profile = tornado.escape.json_decode(response.body)
 
-            # check if user is stored in the database
-            try:
-                stored_user = self.sess.query(User).filter_by(
-                    google_id=user_profile['id']).one()
-                self.set_cookie('user_id', str(stored_user.id))
-                return
-            except NoResultFound:
-                pass
-            except MultipleResultsFound:
-                self.send_error(message='Multiple users found for the given '
-                                        'google_id.')
-
-            # save user here, save to cookie or database
-            new_user = User(
-                first_name=user_profile['given_name'],
-                last_name=user_profile['family_name'],
-                email=user_profile['email'],
-                google_id=user_profile['id']
+            user = get_stored_user_by_3rd_party_id(
+                self, 'google', user_profile['id']
             )
+            if user:
+                set_cookie_with_login_informer(self, user.id)
+                return
 
-            self.sess.add(new_user)
-            self.flush()
-            self.set_cookie('user_id', str(new_user.id))
-            self.sess.commit()
+            new_user_id = store_google_new_user(self, user_profile)
+            set_cookie_with_login_informer(self, str(new_user_id))
             return
 
         yield self.authorize_redirect(
@@ -182,25 +116,23 @@ class GoogleAuthHandler(BaseHandler, GoogleOAuth2Mixin):
 
 class LoginHandler(BaseHandler):
     def post(self):
-        try:
-            stored_user = self.sess.query(User).filter_by(
-                email=self.request.arguments['email'],
-                password=self.request.arguments['password']
-            ).one()
+        login_data = decode_json(self)
+        if login_data is None:
+            # there was an exception while decoding
+            return
 
-            # user_data = {c.name: getattr(stored_user, c.name) for
-            #              c in stored_user.__table__.columns}
+        form = create_user_login_form(login_data, self)
+        if not form:
+            return
 
-        except NoResultFound:
-            self.send_error(400, message='Login failed. Check your email '
-                                         'and password.')
-        except MultipleResultsFound:
-            self.send_error(message='Login failed. Multiple users found '
-                                    'for the given email.')
+        user = get_user_by_email(self, form.email.data)
+        if user and user.password == form.password.data:
+            set_cookie_with_login_informer(self, user.id)
         else:
-            self.set_cookie('user_id', str(stored_user.id))
+            self.send_error(400, message='Invalid email and/or password.')
 
 
 class LogoutHandler(BaseHandler):
     def get(self):
         self.clear_all_cookies()
+        self.send_error(200, message='Logged out.')
--- ecomap/api/v1_0/handlers/base.py
+++ ecomap/api/v1_0/handlers/base.py
@@ -1,29 +1,29 @@
 # coding: utf-8
-
 import tornado.web
 import tornado.escape
-from api.v1_0.models import *
 
+from api.v1_0.models import *
 
 class BaseHandler(tornado.web.RequestHandler):
     def initialize(self):
-        print "session initialized"
         self.sess = self.application.db_sess()
 
     def on_finish(self):
-        print "session closed"
         self.sess.close()
 
     def get_current_user(self):
+        # returns a user id as an int, not as a string
         user_id = self.get_cookie('user_id')
         if not user_id:
-            self.send_error(401, message='You are not authenticated.')
+            self.send_error(401, message='Not authenticated.')
             return
 
         return int(user_id)
 
     def prepare(self):
         # Incorporate request JSON into arguments dictionary.
+        # This method has to be transformed into a simple function and put into
+        # bl package.
         if self.request.body:
             try:
                 json_data = tornado.escape.json_decode(self.request.body)
@@ -33,9 +33,7 @@ class BaseHandler(tornado.web.RequestHandler):
                 self.send_error(400, message=message)
 
     def write_error(self, status_code, **kwargs):
-        error_dict = dict(status_code=status_code)
-        error_dict.update(kwargs)
-        self.write(error_dict)
+        self.write(kwargs)
 
     def get_action_modifier(self):
         current_user = self.sess.query(User).get(self.current_user)
@@ -49,11 +47,15 @@ class BaseHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "*")
         self.set_header("Access-Control-Allow-Credentials", "true")
-        self.set_header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
-        self.set_header("Access-Control-Allow-Headers", "Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, X-Requested-By, If-Modified-Since, X-File-Name, Cache-Control")
+        self.set_header("Access-Control-Allow-Methods",
+                        "GET,PUT,POST,DELETE,OPTIONS")
+        self.set_header(
+            "Access-Control-Allow-Headers",
+            "Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, "
+            "X-Requested-By, If-Modified-Since, X-File-Name, Cache-Control"
+        )
 
     def create_location(self):
         x = self.request.arguments["Latitude"]
         y = self.request.arguments["Longtitude"]
-        return "POINT({0} {1})".format(x,y)
-
+        return "POINT({0} {1})".format(x, y)
