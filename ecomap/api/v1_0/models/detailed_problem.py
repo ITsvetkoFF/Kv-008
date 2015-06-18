de1efb3a491c05b8b4d69e41cdef077926e84308
Bigun <begun.aleksandr@gmail.com>
2015-06-18 13:47:53 +0300

    Bigun commit

--- ecomap/api/v1_0/models/detailed_problem.py
+++ ecomap/api/v1_0/models/detailed_problem.py
@@ -1,5 +1,5 @@
 from api.v1_0.models import *
-from sqlalchemy import *
+from sqlalchemy import*
 from api.v1_0.bl.view_creator import view
 
 metadata = Base.metadata
@@ -7,6 +7,35 @@ user_t = User.__table__.c
 problems_activity_t = ProblemsActivity.__table__.c
 problems_t = Problem.__table__.c
 votes_activity_t = VotesActivity.__table__.c
+
+# query = select(
+#     [
+#         problems_t.id,
+#         problems_t.title,
+#         problems_t.content,
+#         problems_t.proposal,
+#         problems_t.severity,
+#         problems_t.status,
+#         func.ST_GeogFromWKB(problems_t.location).label('location'),
+#         problems_t.problem_type_id,
+#         problems_t.region_id,
+#         problems_activity_t.datetime,
+#         func.count(votes_activity_t.id).label('votes_numbers'),
+#         (user_t.first_name + user_t.last_name).label('name')
+#     ]
+# ).where(
+#     problems_activity_t.problem_id == problems_t.id
+# ).where(
+#     problems_activity_t.user_id == user_t.id
+# ).group_by(
+#     problems_t.id,
+#     problems_activity_t.problem_id,
+#     problems_activity_t.datetime,
+#     user_t.first_name,
+#     user_t.last_name,
+# )
+
+
 
 query = select(
     [
@@ -23,19 +52,21 @@ query = select(
         func.count(votes_activity_t.id).label('votes_numbers'),
         (user_t.first_name + user_t.last_name).label('name')
     ]
-).where(
-    problems_activity_t.problem_id == votes_activity_t.problem_id
-).where(
-    problems_activity_t.problem_id == problems_t.id
-).where(
-    problems_activity_t.user_id == user_t.id
-).group_by(
+)
+
+j = ProblemsActivity.__table__  # Initial table to join.
+table_list = [Problem.__table__, VotesActivity.__table__, User.__table__]
+for table in table_list:
+    j = j.outerjoin(table)
+query = query.select_from(j)
+query = query.group_by(
     problems_t.id,
     problems_activity_t.problem_id,
     problems_activity_t.datetime,
     user_t.first_name,
-    user_t.last_name
+    user_t.last_name,
 )
+
 
 detailed_problem_view = view("detailed_problem", metadata, query)
 
