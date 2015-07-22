95eaa9ad7f440e1ba0984deb7023bc210387d2ba
ITsvetkoFF <tsvetkoff@i.ua>
2015-07-22 13:45:11 +0300

    Update utils.py

--- ecomap/api/v1_0/bl/utils.py
+++ ecomap/api/v1_0/bl/utils.py
@@ -5,7 +5,7 @@ def get_datetime():
 
 def iso_datetime(date):
     if date is not None:
-        return date.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+        return date.strftime("%Y-%m-%dT%H:%M:%S.000%Z")
     else:
         return date
 
