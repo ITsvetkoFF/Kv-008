f84c31a3eda480b9db5ac793d6f661dbdc9efa3e
ITsvetkoFF <tsvetkoff@i.ua>
2015-08-10 11:07:57 +0300

    Update auth.py

--- ecomap/api/v1_0/bl/auth.py
+++ ecomap/api/v1_0/bl/auth.py
@@ -56,7 +56,7 @@ def create_fb_user(user_profile):
         last_name=define_values(user_profile,'last_name'),
         email=user_profile['email'],
         region_id=define_values(user_profile,'region_id'),
-        facebook_id=user_profile['id']
+        facebook_id=user_profile['facebook_id']
     )
 
 
